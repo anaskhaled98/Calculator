f4c7c0d654b843d1a10086d0cb293ce083c49b2b
anas khaled <anaskhaled98@gmail.com>
2024-02-19 21:42:22 +0200

    Excute when the user is writing a number how will update in screen and handling current number. Coding reset and delete operations.

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
-console.log('hello');
+// Light/Dark theme
+const themeElement = document.querySelector(".themes__toggle");
+
+const toggleDarkTheme = () => {
+  themeElement.classList.toggle("themes__toggle--isActive");
+};
+
+const DarkThemeAccissability = (event) => {
+  if (event.key === "Enter") {
+    toggleDarkTheme();
+  }
+};
+
+themeElement.addEventListener("click", toggleDarkTheme);
+themeElement.addEventListener("keydown", DarkThemeAccissability);
+
+// Logic for calculator
+let storedNumber = "";
+let currentNumber = "";
+let operation = "";
+
+const resultElement = document.querySelector(".calc__result");
+const keyElements = document.querySelectorAll("[data-type]");
+
+const updateScreen = (value) => {
+  resultElement.textContent = !value ? "0" : value;
+};
+
+const numberButtonHandler = (value) => {
+  if (value === "." && currentNumber.includes(".")) return;
+  if (value === "0" && !currentNumber) return;
+
+  currentNumber += value;
+  updateScreen(currentNumber);
+};
+
+const resetButtonHandler = () => {
+  storedNumber = "";
+  currentNumber = "";
+  operation = "";
+
+  updateScreen(currentNumber);
+};
+
+const deleteButtonHandler = () => {
+  if (currentNumber) {
+    currentNumber = currentNumber.substring(0, currentNumber.length - 1);
+    updateScreen(currentNumber);
+  }
+};
+
+const keyElementHandler = (Element) => {
+  Element.addEventListener("click", () => {
+    const { type } = Element.dataset;
+    if (type === "number") {
+      numberButtonHandler(Element.dataset.value);
+    } else if (type === "operation") {
+      switch (Element.dataset.value) {
+        case "c":
+          resetButtonHandler();
+          break;
+        case "Backspace":
+          deleteButtonHandler();
+          break;
+      }
+    }
+  });
+};
+
+keyElements.forEach(keyElementHandler);
